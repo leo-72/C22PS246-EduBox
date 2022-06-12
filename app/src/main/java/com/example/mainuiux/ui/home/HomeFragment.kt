57b10a0ed950972f35de2d12859878ad675c5da2
fandi-30 <rizki1900018392@webmail.uad.ac.id>
2022-06-13 06:15:42 +0700

    UI UX UTAMA DAN DATA MATERI SERTA KUIS

---
+++ app/src/main/java/com/example/mainuiux/ui/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.mainuiux.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mainuiux.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+
+        rvMenu.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = HomeAdapter()
+        }
+
+    }
+}
